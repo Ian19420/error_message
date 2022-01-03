cda7adfe3ee8a073d90e8f8a99dc01e7f19d903b
Ian <glps1004@gmail.com>
2022-01-03 23:33:01 +0800

    add computer's moods

--- error_message.py
+++ error_message.py
@@ -1,17 +1,40 @@
 def main():
 	import random
+	import colorama
+	from colorama import Fore, Style, init
+	init(autoreset = True)
 	random_num = [random.randint(1,100) for i in range(10)]
+	moods = 0
 	while True:
-		value = input("position[q to quit]:")
-		if value == "q":
-			print("see u")
+		if moods == 0:
+			value = input("position?[q to quit]:")
+		if moods == 1:
+			print(Fore.YELLOW + Style.DIM + "give u a chance")
+			value = input("what's the position?[q to quit]:")
+		if moods == 2:
+			print(Fore.YELLOW + Style.DIM + "... are u on purpose?")
+			value = input("position!! :( [q to quit]:")
+		if moods == 3 or moods == 4:
+			print(Fore.YELLOW + Style.DIM + "I hate u (-∩-#)")
+			value = input("position[q to quit]:")
+		if moods == 5:
+			import time
+			print(Fore.RED + Style.DIM + "get out!!!")
+			time.sleep(2)
+			print(Fore.RED + Style.DIM + "I just ask u the position, but u r too full :)")
+			time.sleep(2)
 			break
 		try:
+			if value == "q":
+				print("see u")
+				break
 			position = int(value)
-			print(f"{random_num[position]} is in the position, what else?", end = "\t")
+			print(f"{random_num[position]} is in the position")
+			moods = 0
 		except IndexError as err:
-			print(f"It is a bad position {position}, input again?", end = "\t")
+			print(f"It is a bad position {position}")
+			moods += 1
 		except Exception as others:
-			print("someting wrong:", others, "input again?", end = "\t")	
-
+			moods += 1
+			print("someting wrong:", others)	
 main()
